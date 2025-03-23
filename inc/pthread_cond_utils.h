294b8ef8b752a9308ea9543421f2e8c383268b58
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-23 09:22:28 +0530

    all functions will be static inlined, as they all are just wrappers

--- inc/pthread_cond_utils.h
+++ inc/pthread_cond_utils.h
@@ -5,13 +5,13 @@
 #include<pthread.h>
 
 // inits a pthread_cond_t with a CLOCK_MONOTONIC
-int pthread_cond_init_with_monotonic_clock(pthread_cond_t *cond, const pthread_condattr_t *attr);
+static inline int pthread_cond_init_with_monotonic_clock(pthread_cond_t *cond, const pthread_condattr_t *attr);
 
 // below functions can be used to performed a timedwait on a condition variable for a duration given as its last parameter
-int pthread_cond_timedwait_for_timespec(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, struct timespec* duration);
-int pthread_cond_timedwait_for_seconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_seconds);
-int pthread_cond_timedwait_for_milliseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_milliseconds);
-int pthread_cond_timedwait_for_microseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_microseconds);
-int pthread_cond_timedwait_for_nanoseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_nanoseconds);
+static inline int pthread_cond_timedwait_for_timespec(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, struct timespec* duration);
+static inline int pthread_cond_timedwait_for_seconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_seconds);
+static inline int pthread_cond_timedwait_for_milliseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_milliseconds);
+static inline int pthread_cond_timedwait_for_microseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_microseconds);
+static inline int pthread_cond_timedwait_for_nanoseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_nanoseconds);
 
 #endif

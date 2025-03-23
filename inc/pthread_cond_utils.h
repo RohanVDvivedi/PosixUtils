9ae6df0e65b2181ad4b7385ca0eea48199229ab2
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-23 09:30:52 +0530

    added function to initialize clock monotonic cond variables

--- inc/pthread_cond_utils.h
+++ inc/pthread_cond_utils.h
@@ -5,13 +5,28 @@
 #include<pthread.h>
 
 // inits a pthread_cond_t with a CLOCK_MONOTONIC
-static inline int pthread_cond_init_with_monotonic_clock(pthread_cond_t *cond, const pthread_condattr_t *attr);
+static inline int pthread_cond_init_with_monotonic_clock(pthread_cond_t *cond);
 
 // below functions can be used to performed a timedwait on a condition variable for a duration given as its last parameter
+// below functions assume that the condition variable is initialized with CLOCK_MONOTONIC, possibly using the above function (pthread_cond_init_with_monotonic_clock)
 static inline int pthread_cond_timedwait_for_timespec(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, struct timespec* duration);
 static inline int pthread_cond_timedwait_for_seconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_seconds);
 static inline int pthread_cond_timedwait_for_milliseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_milliseconds);
 static inline int pthread_cond_timedwait_for_microseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_microseconds);
 static inline int pthread_cond_timedwait_for_nanoseconds(pthread_cond_t *restrict cond, pthread_mutex_t *restrict mutex, uint64_t* duration_nanoseconds);
+
+static inline int pthread_cond_init_with_monotonic_clock(pthread_cond_t *cond)
+{
+	pthread_condattr_t attr;
+	pthread_cond_condattr_init(&attr);
+
+	pthread_condattr_setclock(&attr, CLOCK_MONOTONIC);
+
+	int result = pthread_cond_init(cond, &attr);
+
+	pthread_cond_condattr_destroy(&attr);
+
+	return result;
+}
 
 #endif

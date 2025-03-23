ed4180d2375e32c61bb5acd4082ad5343084fa49
RohanVDvivedi <rohandvivedi@gmail.com>
2025-03-23 09:38:20 +0530

    setting up readable macros

--- inc/timespec_utils.h
+++ inc/timespec_utils.h
@@ -4,6 +4,12 @@
 #include<stdint.h>
 #include<time.h>
 
+#define MILLISECONDS_IN_SECOND          1000LL
+#define MICROSECONDS_IN_SECOND          1000000LL
+#define NANOSECONDS_IN_SECOND           1000000000LL
+#define NANOSECONDS_IN_MILLISECOND      1000000LL
+#define NANOSECONDS_IN_MICROSECOND      1000LL
+
 static inline struct timespec timespec_from_seconds(uint64_t seconds);
 static inline struct timespec timespec_from_milliseconds(uint64_t milliseconds);
 static inline struct timespec timespec_from_microseconds(uint64_t microseconds);
@@ -20,5 +26,24 @@ static inline struct timespec timespec_add(struct timespec t1, struct timespec t
 static inline struct timespec timespec_sub(struct timespec t1, struct timespec t2);// this function assumesthat t1 > t3
 
 static inline int timespec_compare(struct timespec t1, struct timespec t2);
+
+static inline struct timespec timespec_from_seconds(uint64_t seconds)
+{
+	return struct timespec{
+		.tv_sec = seconds,
+		.tv_nsec = 0,
+	};
+}
+
+static inline struct timespec timespec_from_milliseconds(uint64_t milliseconds)
+{
+	return struct timespec{
+		.tv_sec = seconds,
+		.tv_nsec = 0,
+	};
+}
+
+static inline struct timespec timespec_from_microseconds(uint64_t microseconds);
+static inline struct timespec timespec_from_nanoseconds(uint64_t nanoseconds);
 
 #endif
